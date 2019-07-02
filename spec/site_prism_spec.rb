6e3936976bb9318806df770377853d97a33e29d7
Luke Hill <lukehill_uk@hotmail.com>
2019-07-02 09:54:03 +0200

    Fix requires

--- spec/site_prism_spec.rb
+++ spec/site_prism_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'site_prism'
+# require 'site_prism'
 
 describe SitePrism do
   describe '.configure' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#require 'site_prism'
+require 'site_prism'
 require 'capybara'
 
 $LOAD_PATH << './lib'
