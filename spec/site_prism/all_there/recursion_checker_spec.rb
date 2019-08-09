53ad01e2eb10fe340b99b5361ed81cfc98d7986b
Luke Hill <lukehill_uk@hotmail.com>
2019-08-09 15:23:19 +0200

    Fix up RSpec/ExampleWording & RSpec/VerifiedDoubles

--- spec/site_prism/all_there/recursion_checker_spec.rb
+++ spec/site_prism/all_there/recursion_checker_spec.rb
@@ -1,36 +1,36 @@
 # frozen_string_literal: true
 
 describe 'SitePrism::AllThere::RecursionChecker' do
-  let(:passing_page) { double('A Page with all Items Present', all_there?: true) }
-  let(:failing_page) { double('A Page with some Items Missing', all_there?: false) }
-  let(:passing_section) { double('A Section with all Items Present', all_there?: true) }
-  let(:failing_section) { double('A Section with some Items Missing', all_there?: false) }
+  let(:passing_page) { instance_double('SitePrism::Page', all_there?: true) }
+  let(:failing_page) { instance_double('SitePrism::Page', all_there?: false) }
+  let(:passing_section) { instance_double('SitePrism::Section', all_there?: true) }
+  let(:failing_section) { instance_double('SitePrism::Section', all_there?: false) }
   let(:passing_sections) { [passing_section, passing_section] }
   let(:failing_sections) { [passing_section, failing_section] }
 
   describe '#all_there?' do
     context 'with recursion not set' do
-      it 'should return true for pages that are all_there?' do
+      it 'returns true for pages that are all_there?' do
         expect(passing_page.all_there?).to be true
       end
 
-      it 'should return false for pages that are not all_there?' do
+      it 'returns false for pages that are not all_there?' do
         expect(failing_page.all_there?).to be false
       end
     end
 
     context 'with recursion set to :none' do
-      it 'should return true for pages that are all_there?' do
+      it 'returns true for pages that are all_there?' do
         expect(passing_page.all_there?(recursion: :none)).to be true
       end
 
-      it 'should return false for pages that are not all_there?' do
+      it 'returns false for pages that are not all_there?' do
         expect(failing_page.all_there?(recursion: :none)).to be false
       end
     end
 
     context 'with recursion set to :one' do
-      it 'should return true for pages that have every item and descendent item there' do
+      it 'returns true for pages that have every item and descendent item there' do
         # This allows the Array to be enumerated using #all_there?
         allow_any_instance_of(Array).to receive(:all_there?) do |array|
           array.all?(&:all_there?)
@@ -59,7 +59,7 @@ describe 'SitePrism::AllThere::RecursionChecker' do
         expect(passing_page.all_there?(recursion: :one)).to be true
       end
 
-      it 'should return false for pages that do not have every item and descendent item there' do
+      it 'returns false for pages that do not have every item and descendent item there' do
         # This allows the Array to be enumerated using #all_there?
         allow_any_instance_of(Array).to receive(:all_there?) do |array|
           array.all?(&:all_there?)
