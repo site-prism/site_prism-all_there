78c96b4eb561b2091c53ab652e95e67ee17b145f
Luke Hill <lukehill_uk@hotmail.com>
2019-08-21 17:13:34 +0200

    Update recursion_spec and update rubocop rules

--- spec/site_prism/all_there/recursion_checker_spec.rb
+++ spec/site_prism/all_there/recursion_checker_spec.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-describe 'SitePrism::AllThere::RecursionChecker' do
-  let(:passing_page) { instance_double('SitePrism::Page', all_there?: true) }
-  let(:failing_page) { instance_double('SitePrism::Page', all_there?: false) }
-  let(:passing_section) { instance_double('SitePrism::Section', all_there?: true) }
-  let(:failing_section) { instance_double('SitePrism::Section', all_there?: false) }
+describe SitePrism::AllThere::RecursionChecker do
+  let(:passing_page) { instance_double('SitePrism::Page', 'Passing', all_there?: true) }
+  let(:failing_page) { instance_double('SitePrism::Page', 'Failing', all_there?: false) }
+  let(:passing_section) { instance_double('SitePrism::Section', 'Passing', all_there?: true) }
+  let(:failing_section) { instance_double('SitePrism::Section', 'Failing', all_there?: false) }
   let(:passing_sections) { [passing_section, passing_section] }
   let(:failing_sections) { [passing_section, failing_section] }
 
@@ -30,63 +30,47 @@ describe 'SitePrism::AllThere::RecursionChecker' do
     end
 
     context 'with recursion set to :one' do
-      it 'returns true for pages that have every item and descendent item there' do
-        # This allows the Array to be enumerated using #all_there?
-        allow_any_instance_of(Array).to receive(:all_there?) do |array|
-          array.all?(&:all_there?)
-        end
+      let(:page_there?) { passing_page.all_there? }
+      let(:section_there?) { passing_page.section_classes_to_check.all?(&:all_there?) }
+      let(:sections_there?) { passing_page.sections_classes_to_check.flatten.all?(&:all_there?) }
 
-        # This mocks the passing page having a set of section/sections items
-        # attached to it
-        allow(passing_page)
-          .to receive_messages(
-            section_classes_to_check: passing_sections,
-            sections_classes_to_check: [
-              passing_sections,
-              passing_sections,
-              passing_sections,
-            ]
-          )
+      it 'returns true for pages that have every item and descendent item there' do
+        mock_page_as :passing
 
         # This performs the recursion logic across the current scope, the section
         # items and the sections items
         expect(passing_page).to receive(:all_there?).with(recursion: :one) do
-          passing_page.all_there? &&
-            passing_page.section_classes_to_check.all_there? &&
-            passing_page.sections_classes_to_check.all_there?
+          page_there? && section_there? && sections_there?
         end
 
         expect(passing_page.all_there?(recursion: :one)).to be true
       end
 
       it 'returns false for pages that do not have every item and descendent item there' do
-        # This allows the Array to be enumerated using #all_there?
-        allow_any_instance_of(Array).to receive(:all_there?) do |array|
-          array.all?(&:all_there?)
-        end
-
-        # This mocks the passing page having a set of section/sections items
-        # attached to it
-        allow(passing_page)
-          .to receive_messages(
-            section_classes_to_check: passing_sections,
-            sections_classes_to_check: [
-              passing_sections,
-              failing_sections,
-              passing_sections,
-            ]
-          )
+        mock_page_as :failing
 
         # This performs the recursion logic across the current scope, the section
         # items and the sections items
         expect(passing_page).to receive(:all_there?).with(recursion: :one) do
-          passing_page.all_there? &&
-            passing_page.section_classes_to_check.all_there? &&
-            passing_page.sections_classes_to_check.all_there?
+          page_there? && section_there? && sections_there?
         end
 
         expect(passing_page.all_there?(recursion: :one)).to be false
       end
     end
+  end
+
+  # This mocks the `passing_page` as having a set of section/sections items attached.
+  # Dependent on `type` parameter depends on whether this will pass or fail the #all_there?
+  # check that will be called on passing_page
+  def mock_page_as(type)
+    allow(passing_page)
+      .to receive_messages(
+        section_classes_to_check: passing_sections,
+        sections_classes_to_check: [
+          passing_sections,
+          send("#{type}_sections"),
+        ]
+      )
   end
 end
