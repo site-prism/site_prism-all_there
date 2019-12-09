9bbfe8798a020dd0a16c3e77289c3f8b3e62bbbb
Luke Hill <lukehill_uk@hotmail.com>
2019-12-09 16:44:14 +0100

    Refactor specs to only have one assertion per test
    Add in missing tests from previously

--- spec/site_prism/all_there/recursion_checker_spec.rb
+++ spec/site_prism/all_there/recursion_checker_spec.rb
@@ -10,22 +10,34 @@ describe SitePrism::AllThere::RecursionChecker do
 
   describe '#all_there?' do
     context 'with recursion not set' do
-      it 'returns true for pages that are all_there?' do
+      it 'returns `true` for pages that are `all_there?`' do
         expect(passing_page.all_there?).to be true
       end
 
-      it 'returns false for pages that are not all_there?' do
+      it 'returns `false` for pages that are not `all_there?`' do
         expect(failing_page.all_there?).to be false
+      end
+
+      it 'does not perform checks on descendant section/s' do
+        expect(passing_page).not_to receive(:all_there?) do
+          section_there? && sections_there?
+        end
       end
     end
 
     context 'with recursion set to :none' do
-      it 'returns true for pages that are all_there?' do
+      it 'returns `true` for pages that are `all_there?`' do
         expect(passing_page.all_there?(recursion: :none)).to be true
       end
 
-      it 'returns false for pages that are not all_there?' do
+      it 'returns `false` for pages that are not `all_there?`' do
         expect(failing_page.all_there?(recursion: :none)).to be false
+      end
+
+      it 'does not perform checks on descendant section/s' do
+        expect(passing_page).not_to receive(:all_there?).with(recursion: :one) do
+          section_there? && sections_there?
+        end
       end
     end
 
@@ -34,26 +46,31 @@ describe SitePrism::AllThere::RecursionChecker do
       let(:section_there?) { passing_page.section_classes_to_check.all?(&:all_there?) }
       let(:sections_there?) { passing_page.sections_classes_to_check.flatten.all?(&:all_there?) }
 
-      it 'returns true for pages that have every item and descendent item there' do
-        mock_page_as :passing
-
-        # This performs the recursion logic across the current scope, the section
-        # items and the sections items
-        expect(passing_page).to receive(:all_there?).with(recursion: :one) do
+      before do
+        # Set the `all_there?` check to be the legit one that recurses
+        allow(passing_page).to receive(:all_there?).with(recursion: :one) do
           page_there? && section_there? && sections_there?
         end
+      end
+
+      it 'returns `true` for pages that have every item and descendant item present' do
+        mock_page_as :passing
 
         expect(passing_page.all_there?(recursion: :one)).to be true
       end
 
-      it 'returns false for pages that do not have every item and descendent item there' do
-        mock_page_as :failing
+      it 'performs checks on both the current page, as well as all descendant section/s' do
+        mock_page_as :passing
 
-        # This performs the recursion logic across the current scope, the section
-        # items and the sections items
         expect(passing_page).to receive(:all_there?).with(recursion: :one) do
           page_there? && section_there? && sections_there?
         end
+
+        passing_page.all_there?(recursion: :one)
+      end
+
+      it 'returns `false` for pages that do not have every item and descendant item present' do
+        mock_page_as :failing
 
         expect(passing_page.all_there?(recursion: :one)).to be false
       end
@@ -61,7 +78,7 @@ describe SitePrism::AllThere::RecursionChecker do
   end
 
   # This mocks the `passing_page` as having a set of section/sections items attached.
-  # Dependent on `type` parameter depends on whether this will pass or fail the #all_there?
+  # Dependent on `type` parameter depends on whether this will pass or fail the +#all_there?+
   # check that will be called on passing_page
   def mock_page_as(type)
     allow(passing_page)
