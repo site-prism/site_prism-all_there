5aac29e9feb8589670070ddf65439a878e8fa351
Luke Hill <lukehill_uk@hotmail.com>
2022-12-07 11:29:35 +0000

    Page mocking is no longer needed!

--- spec/support/shared_context/page_types.rb
+++ spec/support/shared_context/page_types.rb
@@ -3,18 +3,4 @@
 RSpec.shared_context 'with mock pages' do
   let(:passing_page) { instance_double('SitePrism::Page', 'Passing') }
   let(:failing_page) { instance_double('SitePrism::Page', 'Failing') }
-
-  # This mocks the `passing_page` as having a set of section/sections items attached.
-  # Dependent on `type` parameter depends on whether this will pass or fail the +#all_there?+
-  # check that will be called on passing_page
-  def mock_page_as(type)
-    allow(passing_page)
-      .to receive_messages(
-        section_classes_to_check: passing_sections,
-        sections_classes_to_check: [
-          passing_sections,
-          send("#{type}_sections"),
-        ]
-      )
-  end
 end
