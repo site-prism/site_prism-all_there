b00e4b30b33f82d1298fbff2f7d97f1a6c1ac014
Luke Hill <lukehill_uk@hotmail.com>
2022-12-07 11:17:33 +0000

    move mock page as into page context

--- spec/site_prism/all_there/recursion_checker_spec.rb
+++ spec/site_prism/all_there/recursion_checker_spec.rb
@@ -115,18 +115,4 @@ describe SitePrism::AllThere::RecursionChecker do
       end
     end
   end
-
-  # This mocks the `passing_page` as having a set of section/sections items attached.
-  # Dependent on `type` parameter depends on whether this will pass or fail the +#all_there?+
-  # check that will be called on passing_page
-  def mock_page_as(type)
-    allow(passing_page)
-      .to receive_messages(
-        section_classes_to_check: passing_sections,
-        sections_classes_to_check: [
-          passing_sections,
-          send("#{type}_sections"),
-        ]
-      )
-  end
 end
