3977794ca44ebdd9d1d673c547272a2e181273b3
Luke Hill <lukehill_uk@hotmail.com>
2022-12-07 11:40:22 +0000

    Completely remove mock sections - again we have specific interception points inside the all_there? recursion/non recursion checks to handle use cases

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,7 @@ $LOAD_PATH << './lib'
 
 require 'site_prism/all_there'
 require_relative 'support/shared_context/page_types'
-require_relative 'support/shared_context/section_types'
 
 RSpec.configure do |config|
   config.include_context('with mock pages')
-  config.include_context('with mock sections')
 end
--- spec/support/shared_context/section_types.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.shared_context 'with mock sections' do
-  let(:passing_sections) { [passing_section, passing_section] }
-  let(:failing_sections) { [passing_section, failing_section] }
-
-  let(:passing_section) { instance_double('SitePrism::Section', 'Passing', all_there?: true) }
-  let(:failing_section) { instance_double('SitePrism::Section', 'Failing', all_there?: false) }
-end
