35440b0305b7ba4222f2c4ca0caf4533df614f25
Luke Hill <20105237+luke-hill@users.noreply.github.com>
2023-06-22 14:04:25 +0100

    Update mapped items optimisation (#53)
    * Mapped items class by default stores the mapped items on the instance
    
    * Need to access hash outside of execution
    
    * Add changelog

--- lib/site_prism/all_there/mapped_items.rb
+++ lib/site_prism/all_there/mapped_items.rb
@@ -8,23 +8,16 @@ module SitePrism
     # The Expected Items on a SitePrism Page or Section structured in an enumerable way
     #
     class MappedItems
-      attr_reader :instance
-      private :instance
+      attr_reader :hash
 
       def initialize(instance)
-        @instance = instance
+        @hash = instance.class.mapped_items
       end
 
       # @return [Array<Symbol>]
       # All expected mapped items on the SitePrism instance as a Symbol Array
       def array
         hash.values.flatten.uniq
-      end
-
-      # @return [Hash<Symbol>]
-      # All expected mapped items on the SitePrism instance as a Symbol Hash
-      def hash
-        instance.class.mapped_items
       end
     end
   end
