d3896a3f76207269b356074b54beda214247e62d
Luke Hill <lukehill_uk@hotmail.com>
2023-07-13 15:30:02 +0100

    Remove fallbacks for individual mapped checklists (No longer needed as of v4 site_prism)
    Remove un-used methods and absorb into #array

--- lib/site_prism/all_there/expected_items.rb
+++ lib/site_prism/all_there/expected_items.rb
@@ -19,46 +19,28 @@ module SitePrism
       # All defined/expected mapped items
       def array
         [
-          element,
-          elements,
+          mapped_checklist_of(:element),
+          mapped_checklist_of(:elements),
           section,
           sections,
-          iframe
+          mapped_checklist_of(:iframe)
         ]
-      end
-
-      # @return [Array<Symbol>]
-      # All defined/expected items that were mapped as +element+
-      def element
-        mapped_checklist_of(:element) || []
-      end
-
-      # @return [Array<Symbol>]
-      # All defined/expected items that were mapped as +elements+
-      def elements
-        mapped_checklist_of(:elements) || []
       end
 
       # @return [Array<Symbol>]
       # All defined/expected items that were mapped as +section+
       def section
-        mapped_checklist_of(:section) || []
+        mapped_checklist_of(:section)
       end
 
       # @return [Array<Symbol>]
       # All defined/expected items that were mapped as +sections+
       def sections
-        mapped_checklist_of(:sections) || []
-      end
-
-      # @return [Array<Symbol>]
-      # All defined/expected items that were mapped as +iframe+
-      def iframe
-        mapped_checklist_of(:iframe) || []
+        mapped_checklist_of(:sections)
       end
 
       private
-      
+
       def mapped_checklist_of(type)
         mapped_items.hash[type] & mapped_checklist
       end
